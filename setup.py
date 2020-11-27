320dc96c04ffc0f23a59ceb1d8a982b77898142b
ycjungSubhuman <ycjung@postech.ac.kr>
2020-11-27 23:44:19 +0900

    Up the version

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from Cython.Build import cythonize
 setup(
     name='cyobj',
     description='A fast and Wavefront .OBJ reader/writer',
-    version="0.1.0",
+    version="0.2.0",
     author='Yucheol Jung',
     author_email='ycjung@postech.ac.kr',
     packages=['cyobj'],
     url='https://github.com/ycjungSubhuman/cyobj',
-    ext_modules=cythonize("cyobj/io.pyx",
+    ext_modules=cythonize("cyobj/detail/io.pyx",
                           compiler_directives={'language_level': "3"}),
 )
